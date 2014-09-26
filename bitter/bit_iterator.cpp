cce3634620f119e9c8f886473900e04e8397e9bf
Jeff Snyder <jeff-github@caffeinated.me.uk>
2014-09-26 20:15:14 +0100

    Expand bit_iterator tests

--- bitter/bit_iterator.cpp
+++ bitter/bit_iterator.cpp
@@ -3,6 +3,9 @@ using namespace bandit;
 
 #include "bit_iterator.hpp"
 
+const unsigned char* operator "" _uc(const char* str, std::size_t len)
+{ return reinterpret_cast<const unsigned char*>(str); }
+
 using bitvec = std::vector<bitter::bit>;
 bitvec make_bitvec(std::initializer_list<int> il)
 {
@@ -18,11 +21,11 @@ bitvec make_bitvec(std::initializer_list<int> il)
 }
 
 template <std::size_t N>
-std::string hexstring(char (&data)[N])
+std::string hexstring(uint8_t (&data)[N])
 {
     char ret[N*2];
     char* out = ret;
-    std::for_each(data, data+N, [&](char c){
+    std::for_each(data, data+N, [&](uint8_t c){
         std::sprintf(out, "%02X", static_cast<uint8_t>(c));
         out += 2;
     });
@@ -34,13 +37,13 @@ struct ArrayDelete {
     void operator()(T*t) { delete[] t; }
 };
 
-std::string hexstring(const std::uint8_t *data, std::size_t n)
+std::string hexstring(uint8_t *data, std::size_t n)
 {
     const auto sz = (n+7)/8;
     const auto retlen = 2*sz;
     std::vector<char> ret(retlen+1);
     char* out = ret.data();
-    std::for_each(data, data+sz, [&](char c){
+    std::for_each(data, data+sz, [&](uint8_t c){
         std::sprintf(out, "%02X", static_cast<uint8_t>(c));
         out += 2;
     });
@@ -51,95 +54,148 @@ std::string hexstring(const std::uint8_t *data, std::size_t n)
 template <typename OutIt>
 std::string copy_and_hexify(const bitvec& source, std::ptrdiff_t n)
 {
-    std::vector<std::uint8_t> out((n+7)/8);
+    std::vector<uint8_t> out((n+7)/8);
     std::copy(begin(source), next(begin(source), n), OutIt(out.data(), 0));
     return hexstring(out.data(), n);
 }
 
-go_bandit([]{
-    using lsb0iter = bitter::bit_iterator<bitter::bit_order::lsb0>;
-    using msb0iter = bitter::bit_iterator<bitter::bit_order::msb0>;
-    using lsb0citer = bitter::const_bit_iterator<bitter::bit_order::lsb0>;
-    using msb0citer = bitter::const_bit_iterator<bitter::bit_order::msb0>;
-    const bitvec bits16(16, bitter::bit(1));
+template <bitter::bit_order BO>
+void core_tests()
+{
+    static_assert(BO==bitter::bit_order::msb0 or BO==bitter::bit_order::lsb0,
+            "unexpected bit order");
+    using iter = bitter::bit_iterator<BO>;
+    using citer = bitter::const_bit_iterator<BO>;
 
-    it("compares equal to self when default constructed", []{
-        lsb0iter l1;
-        lsb0iter l2;
-        AssertThat(l1, Equals(l2));
-        msb0iter m1;
-        msb0iter m2;
-        AssertThat(m1, Equals(m2));
+    it("supports default construction", []{
+        iter i1;
+        (void) i1;
+    });
+
+    it("support comparison of default constructed objects", [] {
+        iter i1;
+        iter i2;
+        AssertThat(i1 == i2, Equals(true));
+        AssertThat(i1 != i2, Equals(false));
+    });
+
+    it("supports copy-construction", []{
+        uint8_t foo[8];
+        for (std::size_t c = 0; c < sizeof(foo)/sizeof(uint8_t); ++c)
+        {
+            for (std::size_t b = 0; b < 8; ++b)
+            {
+                iter i1(&foo[c], b);
+                iter i2(i1);
+                AssertThat(i1, Equals(i2));
+            }
+        }
+    });
+
+    it("supports assignment", []{
+        uint8_t foo[8];
+        iter i1;
+        for (std::size_t c = 0; c < sizeof(foo)/sizeof(uint8_t); ++c)
+        {
+            for (std::size_t b = 0; b < 8; ++b)
+            {
+                iter i2(&foo[c], b);
+                i1 = i2;
+                AssertThat(i1, Equals(i2));
+            }
+        }
+    });
+
+    it("supports preincrement", []{
+        uint8_t foo[8];
+        for (std::size_t c = 0; c < sizeof(foo)/sizeof(uint8_t); ++c)
+        {
+            for (std::size_t b = 0; b < 8; ++b)
+            {
+                iter i1(&foo[c], b);
+                iter i2(&foo[c+(b+1)/8], (b+1)%8);
+                AssertThat(++i1, Equals(i2));
+                AssertThat(i1, Equals(i2));
+            }
+        }
+    });
+
+/*    it("supports postincrement", []{
+        uint8_t foo[8];
+        for (std::size_t c = 0; c < sizeof(foo)/sizeof(uint8_t); ++c)
+        {
+            for (std::size_t b = 0; b < 8; ++b)
+            {
+                iter i1(&foo[c], b);
+                iter i2(&foo[c], b);
+                iter i3(&foo[c+(b+1)/8], (b+1)%8);
+                AssertThat(i1++, Equals(i1));
+                AssertThat(i1, Equals(i2));
+            }
+        }
+    });*/
+
+    it("supports equality/inequality comparison", []{
+        uint8_t foo[8];
+        for (std::size_t c1 = 0; c1 < sizeof(foo)/sizeof(uint8_t); ++c1)
+        {
+            for (std::size_t b1 = 0; b1 < 8; ++b1)
+            {
+                iter i1(&foo[c1], b1);
+                for (std::size_t c2 = 0; c2 < sizeof(foo)/sizeof(uint8_t); ++c2)
+                {
+                    for (std::size_t b2 = 0; b2 < 8; ++b2)
+                    {
+                        iter i2(&foo[c2], b2);
+                        AssertThat(i1 == i2, Equals(c1 == c2 and b1 == b2));
+                        AssertThat(i1 != i2, Equals(c1 != c2 or b1 != b2));
+                    }
+                }
+            }
+        }
     });
 
     it("satisfies the examples in NIST FIPS 202", []{
-        const std::uint8_t data[] = {0xA3u};
-        const bitvec interpreted_as_msb0 = make_bitvec({1,0,1,0, 0,0,1,1});
-        const bitvec interpreted_as_lsb0 = make_bitvec({1,1,0,0, 0,1,0,1});
+        const std::array<uint8_t,1> data = {{0xA3}};
+        const bitvec interpreted_as_bits =
+            (BO == bitter::bit_order::msb0)
+                ? make_bitvec({1,0,1,0, 0,0,1,1})
+                : make_bitvec({1,1,0,0, 0,1,0,1});
 
-        bitvec converted_to_msb0;
-        std::copy(msb0citer(data, 0), msb0citer(data+1, 0),
-                  back_inserter(converted_to_msb0));
-        AssertThat(converted_to_msb0, EqualsContainer(interpreted_as_msb0));
+        bitvec converted_to_bits;
+        std::copy(citer(begin(data), 0), citer(end(data), 0),
+                  back_inserter(converted_to_bits));
+        AssertThat(converted_to_bits, EqualsContainer(interpreted_as_bits));
 
-        bitvec converted_to_lsb0;
-        std::copy(lsb0citer(data, 0), lsb0citer(data+1, 0),
-                  back_inserter(converted_to_lsb0));
-        AssertThat(converted_to_lsb0, EqualsContainer(interpreted_as_lsb0));
-
-        std::uint8_t data_m[] = {0,0};
-        std::copy(begin(interpreted_as_msb0), end(interpreted_as_msb0),
-                  msb0iter(data_m,0));
-        AssertThat(static_cast<uint8_t>(data_m[0]), Equals(0xa3));
-        AssertThat(static_cast<uint8_t>(data_m[1]), Equals(0));
-
-        std::uint8_t data_l[] = {0,0};
-        std::copy(begin(interpreted_as_lsb0), end(interpreted_as_lsb0),
-                  lsb0iter(data_l,0));
-        AssertThat(static_cast<uint8_t>(data_l[0]), Equals(0xa3));
-        AssertThat(static_cast<uint8_t>(data_l[1]), Equals(0));
+        unsigned char data_out[] = {0,0};
+        std::copy(begin(interpreted_as_bits), end(interpreted_as_bits),
+                  iter(data_out,0));
+        AssertThat(static_cast<uint8_t>(data_out[0]), Equals(0xa3));
+        AssertThat(static_cast<uint8_t>(data_out[1]), Equals(0));
     });
 
-    it("handles partial bytes with msb0", [&]{
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 0),  Equals(""));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 1),  Equals("80"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 2),  Equals("C0"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 3),  Equals("E0"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 4),  Equals("F0"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 5),  Equals("F8"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 6),  Equals("FC"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 7),  Equals("FE"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 8),  Equals("FF"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 9),  Equals("FF80"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 10), Equals("FFC0"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 11), Equals("FFE0"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 12), Equals("FFF0"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 13), Equals("FFF8"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 14), Equals("FFFC"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 15), Equals("FFFE"));
-        AssertThat(copy_and_hexify<msb0iter>(bits16, 16), Equals("FFFF"));
+    it("handles partial bytes", [&]{
+        std::vector<const char*> results =
+            (BO == bitter::bit_order::msb0)
+                ? std::vector<const char*>{"",
+                                           "80",   "C0",   "E0",   "F0",
+                                           "F8",   "FC",   "FE",   "FF",
+                                           "FF80", "FFC0", "FFE0", "FFF0",
+                                           "FFF8", "FFFC", "FFFE", "FFFF"}
+                : std::vector<const char*>{"",
+                                           "01",   "03",   "07",   "0F",
+                                           "1F",   "3F",   "7F",   "FF",
+                                           "FF01", "FF03", "FF07", "FF0F",
+                                           "FF1F", "FF3F", "FF7F", "FFFF"};
+        const bitvec bits16(16, bitter::bit(1));
+        for (int i = 0; i <= 16; ++i)
+            AssertThat(copy_and_hexify<iter>(bits16, i), Equals(results.at(i)));
     });
+}
 
-    it("handles partial bytes with lsb0", [&]{
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 0),  Equals(""));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 1),  Equals("01"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 2),  Equals("03"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 3),  Equals("07"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 4),  Equals("0F"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 5),  Equals("1F"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 6),  Equals("3F"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 7),  Equals("7F"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 8),  Equals("FF"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 9),  Equals("FF01"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 10), Equals("FF03"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 11), Equals("FF07"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 12), Equals("FF0F"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 13), Equals("FF1F"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 14), Equals("FF3F"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 15), Equals("FF7F"));
-        AssertThat(copy_and_hexify<lsb0iter>(bits16, 16), Equals("FFFF"));
-    });
-
+go_bandit([]{
+    describe("bit_iterator<bitter::bit_order::msb0>", &core_tests<bitter::bit_order::msb0>);
+    describe("bit_iterator<bitter::bit_order::lsb0>", &core_tests<bitter::bit_order::lsb0>);
 });
 
 int main(int argc, char** argv)

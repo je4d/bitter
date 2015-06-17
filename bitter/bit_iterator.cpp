71ce18e4c5cf85828f7a2b960145209bb9ade2f6
Jeff Snyder <jeff-github@caffeinated.me.uk>
2015-06-17 20:10:44 +0100

    Drop the testvec size to 128 bits

--- bitter/bit_iterator.cpp
+++ bitter/bit_iterator.cpp
@@ -112,7 +112,7 @@ std::string hexstring(const UL *data, std::size_t n)
     return std::string(ret.data(), retlen);
 }
 
-static constexpr std::ptrdiff_t testvec_bits = 32*8;
+static constexpr std::ptrdiff_t testvec_bits = 32*4;
 
 template <bit_order BO, typename UL, byte_order YO = byte_order::none>
 struct testvec_;
@@ -120,89 +120,73 @@ struct testvec_;
 template<> struct testvec_<bit_order::lsb0,uint8_t> {
     static constexpr std::array<uint8_t,testvec_bits/8> data {{
         0xa2, 0x2b, 0x34, 0x6c, 0xc9, 0xf8, 0x1f, 0xce,
-        0x4c, 0xd2, 0x4a, 0x55, 0x55, 0xb5, 0x5a, 0x6b,
-        0x69, 0x4b, 0x92, 0x6d, 0xb2, 0x99, 0xcc, 0xcc,
-        0xcc, 0xcc, 0x8c, 0x19, 0x73, 0xce, 0x18, 0xe7 }}; };
+        0x4c, 0xd2, 0x4a, 0x55, 0x55, 0xb5, 0x5a, 0x6b }}; };
 constexpr std::array<uint8_t,testvec_bits/8>
      testvec_<bit_order::lsb0,uint8_t>::data;
 template<> struct testvec_<bit_order::lsb0,uint16_t,byte_order::lsb0> {////
     static constexpr std::array<uint16_t,testvec_bits/16> data {{
-        0x2ba2, 0x6c34, 0xf8c9, 0xce1f, 0xd24c, 0x554a, 0xb555, 0x6b5a,
-        0x4b69, 0x6d92, 0x99b2, 0xcccc, 0xcccc, 0x198c, 0xce73, 0xe718 }}; };
+        0x2ba2, 0x6c34, 0xf8c9, 0xce1f, 0xd24c, 0x554a, 0xb555, 0x6b5a }}; };
 constexpr std::array<uint16_t,testvec_bits/16>
      testvec_<bit_order::lsb0,uint16_t,byte_order::lsb0>::data;
 template<> struct testvec_<bit_order::lsb0,uint32_t,byte_order::lsb0> {
     static constexpr std::array<uint32_t,testvec_bits/32> data {{
-        0x6c342ba2, 0xce1ff8c9, 0x554ad24c, 0x6b5ab555,
-        0x6d924b69, 0xcccc99b2, 0x198ccccc, 0xe718ce73 }}; };
+        0x6c342ba2, 0xce1ff8c9, 0x554ad24c, 0x6b5ab555 }}; };
 constexpr std::array<uint32_t,testvec_bits/32>
      testvec_<bit_order::lsb0,uint32_t,byte_order::lsb0>::data;
 template<> struct testvec_<bit_order::lsb0,uint64_t,byte_order::lsb0> {
     static constexpr std::array<uint64_t,testvec_bits/64> data {{
-        0xce1ff8c96c342ba2, 0x6b5ab555554ad24c,
-        0xcccc99b26d924b69, 0xe718ce73198ccccc }}; };
+        0xce1ff8c96c342ba2, 0x6b5ab555554ad24c }}; };
 constexpr std::array<uint64_t,testvec_bits/64>
      testvec_<bit_order::lsb0,uint64_t,byte_order::lsb0>::data;
 template<> struct testvec_<bit_order::lsb0,uint16_t,byte_order::msb0> {
     static constexpr std::array<uint16_t,testvec_bits/16> data {{
-        0xa22b, 0x346c, 0xc9f8, 0x1fce, 0x4cd2, 0x4a55, 0x55b5, 0x5a6b,
-        0x694b, 0x926d, 0xb299, 0xcccc, 0xcccc, 0x8c19, 0x73ce, 0x18e7 }}; };
+        0xa22b, 0x346c, 0xc9f8, 0x1fce, 0x4cd2, 0x4a55, 0x55b5, 0x5a6b }}; };
 constexpr std::array<uint16_t,testvec_bits/16>
      testvec_<bit_order::lsb0,uint16_t,byte_order::msb0>::data;
 template<> struct testvec_<bit_order::lsb0,uint32_t,byte_order::msb0> {
     static constexpr std::array<uint32_t,testvec_bits/32> data {{
-        0xa22b346c, 0xc9f81fce, 0x4cd24a55, 0x55b55a6b,
-        0x694b926d, 0xb299cccc, 0xcccc8c19, 0x73ce18e7 }}; };
+        0xa22b346c, 0xc9f81fce, 0x4cd24a55, 0x55b55a6b }}; };
 constexpr std::array<uint32_t,testvec_bits/32>
      testvec_<bit_order::lsb0,uint32_t,byte_order::msb0>::data;
 template<> struct testvec_<bit_order::lsb0,uint64_t,byte_order::msb0> {
     static constexpr std::array<uint64_t,testvec_bits/64> data {{
-        0xa22b346cc9f81fce, 0x4cd24a5555b55a6b,
-        0x694b926db299cccc, 0xcccc8c1973ce18e7 }}; };
+        0xa22b346cc9f81fce, 0x4cd24a5555b55a6b }}; };
 constexpr std::array<uint64_t,testvec_bits/64>
      testvec_<bit_order::lsb0,uint64_t,byte_order::msb0>::data;
 template<> struct testvec_<bit_order::msb0,uint8_t> {
     static constexpr std::array<uint8_t,testvec_bits/8> data {{
         0x45, 0xd4, 0x2c, 0x36, 0x93, 0x1f, 0xf8, 0x73,
-        0x32, 0x4b, 0x52, 0xaa, 0xaa, 0xad, 0x5a, 0xd6,
-        0x96, 0xd2, 0x49, 0xb6, 0x4d, 0x99, 0x33, 0x33,
-        0x33, 0x33, 0x31, 0x98, 0xce, 0x73, 0x18, 0xe7 }}; };
+        0x32, 0x4b, 0x52, 0xaa, 0xaa, 0xad, 0x5a, 0xd6 }}; };
 constexpr std::array<uint8_t,testvec_bits/8>
      testvec_<bit_order::msb0,uint8_t>::data;
 template<> struct testvec_<bit_order::msb0,uint16_t,byte_order::lsb0> {
     static constexpr std::array<uint16_t,testvec_bits/16> data {{
-        0xd445, 0x362c, 0x1f93, 0x73f8, 0x4b32, 0xaa52, 0xadaa, 0xd65a,
-        0xd296, 0xb649, 0x994d, 0x3333, 0x3333, 0x9831, 0x73ce, 0xe718 }}; };
+        0xd445, 0x362c, 0x1f93, 0x73f8, 0x4b32, 0xaa52, 0xadaa, 0xd65a }}; };
 constexpr std::array<uint16_t,testvec_bits/16>
      testvec_<bit_order::msb0,uint16_t,byte_order::lsb0>::data;
 template<> struct testvec_<bit_order::msb0,uint32_t,byte_order::lsb0> {
     static constexpr std::array<uint32_t,testvec_bits/32> data {{
-        0x362cd445, 0x73f81f93, 0xaa524b32, 0xd65aadaa,
-        0xb649d296, 0x3333994d, 0x98313333, 0xe71873ce }}; };
+        0x362cd445, 0x73f81f93, 0xaa524b32, 0xd65aadaa }}; };
 constexpr std::array<uint32_t,testvec_bits/32>
      testvec_<bit_order::msb0,uint32_t,byte_order::lsb0>::data;
 template<> struct testvec_<bit_order::msb0,uint64_t,byte_order::lsb0> {
     static constexpr std::array<uint64_t,testvec_bits/64> data {{
-        0x73f81f93362cd445, 0xd65aadaaaa524b32,
-        0x3333994db649d296, 0xe71873ce98313333 }}; };
+        0x73f81f93362cd445, 0xd65aadaaaa524b32 }}; };
 constexpr std::array<uint64_t,testvec_bits/64>
      testvec_<bit_order::msb0,uint64_t,byte_order::lsb0>::data;
 template<> struct testvec_<bit_order::msb0,uint16_t,byte_order::msb0> {
     static constexpr std::array<uint16_t,testvec_bits/16> data {{
-        0x45d4, 0x2c36, 0x931f, 0xf873, 0x324b, 0x52aa, 0xaaad, 0x5ad6,
-        0x96d2, 0x49b6, 0x4d99, 0x3333, 0x3333, 0x3198, 0xce73, 0x18e7 }}; };
+        0x45d4, 0x2c36, 0x931f, 0xf873, 0x324b, 0x52aa, 0xaaad, 0x5ad6 }}; };
 constexpr std::array<uint16_t,testvec_bits/16>
      testvec_<bit_order::msb0,uint16_t,byte_order::msb0>::data;
 template<> struct testvec_<bit_order::msb0,uint32_t,byte_order::msb0> {
     static constexpr std::array<uint32_t,testvec_bits/32> data {{
-        0x45d42c36, 0x931ff873, 0x324b52aa, 0xaaad5ad6,
-        0x96d249b6, 0x4d993333, 0x33333198, 0xce7318e7 }}; };
+        0x45d42c36, 0x931ff873, 0x324b52aa, 0xaaad5ad6 }}; };
 constexpr std::array<uint32_t,testvec_bits/32>
      testvec_<bit_order::msb0,uint32_t,byte_order::msb0>::data;
 template<> struct testvec_<bit_order::msb0,uint64_t,byte_order::msb0> {
     static constexpr std::array<uint64_t,testvec_bits/64> data {{
-        0x45d42c36931ff873, 0x324b52aaaaad5ad6,
-        0x96d249b64d993333, 0x33333198ce7318e7 }}; };
+        0x45d42c36931ff873, 0x324b52aaaaad5ad6 }}; };
 constexpr std::array<uint64_t,testvec_bits/64>
      testvec_<bit_order::msb0,uint64_t,byte_order::msb0>::data;
 
